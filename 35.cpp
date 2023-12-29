c71b54f5d71719d84118238d2c86214b125a4347
حسین ایمان نژاد <150318801+Imannezhad40216341054552@users.noreply.github.com>
2023-12-29 23:01:53 +0330

    Add files via upload

---
+++ 35.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    float x, y;
+      cin >> x >> y;
+      
+      cout << sin(sqrt((log(x + y * x * x)) / (pow(x, 3) + sqrt(x + y * y))));
+  return 0;
+}
---
+++ 38.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int i;
+ 	char h;
+ 	for( i=1;i<=122;i++)
+ 	{
+ 	h=1;
+	 cout<<i<<"="<<h<<endl;
+	 	
+	}
+ return 0 ;
+ }
